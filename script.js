04610ec3b5b558668aad3a836586b6d2413952f5
restuchan <restu.chan188@gmail.com>
2026-03-14 20:04:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Date() untuk menampilkan tanggal saat ini
+Date()
+
+//mendapatkan tanggal saat ini
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = tanggal
+
+//untuk menampilkan alert tampil()
+function tampil() {
+    alert( tanggal)
+}
+
+//untuk menampilkan alert show()
+
+function show() {
+    alert("Hallo, Terima kasih telah mengunjungi Sawit Persada Sejahtera. Ada yang bisa kami bantu?")
+}
